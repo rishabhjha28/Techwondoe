debfa61368ba12078d31160ea7b8afc8db084b23
Rishabh Jha <rishabhanandjha@gmail.com>
2023-01-14 04:21:04 +0530

    made changes for deployment

--- server/index.js
+++ server/index.js
@@ -17,10 +17,10 @@ app.use('/count', countRouter);
 
 if(process.env.NODE_ENV=="production"){
     console.log('abc')
-    app.use(express.static('client/build'))
+    app.use(express.static('../client/build'))
     const path = require('path')
     app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+        res.sendFile(path.resolve(__dirname,'../client','build','index.html'))
     })
 }
 
